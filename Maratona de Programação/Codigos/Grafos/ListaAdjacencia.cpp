646758389b208d1eccc9ada24055b2e24e7ccfec
marcuscxb <markinho_cxb@hotmail.com>
2015-04-14 11:57:14 -0300

    Primeiro Commit

---
+++ Maratona de Programação/Codigos/Grafos/ListaAdjacencia.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define MAXV 100 // quantidade de vértices
+
+vector<int> ladj[MAXV]; // Lista de adjacência não ponderada (Origem -> Destino)
+vector< pair<int,int> > ladjP[MAXV]; // Lista de adjacência ponderada (Origem, Peso - > Destino)
+
+int main()
+{
+    /*
+
+       1 ----- 2 ----- 4
+       |
+       |
+       |
+       |
+       3
+     */
+
+	return 0;
+}
